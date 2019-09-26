96b525ee72a09d839883654c00b7d29fe905f09f
kimulaco <kmrkdev@gmail.com>
2019-09-26 09:25:13 +0900

    Add cli watch test

--- test/broc-cli.test.ts
+++ test/broc-cli.test.ts
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import { generate } from '../src/broc-cli'
+import { generate, watch } from '../src/broc-cli'
 import { Blog, Post, Tags } from '../src/types'
 import { hasOwnProperties } from './utils/utils'
 
@@ -8,6 +8,53 @@ describe('generate', (): void => {
 
   beforeAll(async () => {
     await generate('./test/resources', './test/cli/blog.json')
+    blog = JSON.parse(fs.readFileSync('./test/cli/blog.json').toString())
+  })
+
+  test('posts length', () => {
+    expect(blog.posts.length).toBe(2)
+  })
+
+  test('has meta', () => {
+    const post: Post = blog.posts[0]
+    expect(hasOwnProperties(post.meta, [
+      'id',
+      'title',
+      'description',
+      'category',
+      'tags',
+      'status',
+      'created_at',
+      'updated_at'
+    ])).toBeTruthy()
+  })
+
+  test('has html body', () => {
+    const post: Post = blog.posts[0]
+    expect(post.body.html).toBe('<h1>test-02</h1>\n')
+  })
+
+  test('has md body', () => {
+    const post: Post = blog.posts[0]
+    expect(post.body.md).toBe('\n# test-02')
+  })
+
+  test('has text body', () => {
+    const post: Post = blog.posts[0]
+    expect(post.body.text).toBe('\ntest-02')
+  })
+
+  test('tags length', () => {
+    const tags: Tags = blog.tags
+    expect(tags.length).toBe(6)
+  })
+})
+
+describe('watch', (): void => {
+  let blog: Blog
+
+  beforeAll(async () => {
+    await watch('./test/resources', './test/cli/blog.json')
     blog = JSON.parse(fs.readFileSync('./test/cli/blog.json').toString())
   })
 
